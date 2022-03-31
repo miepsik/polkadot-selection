86a233551efdfaa52610aa9ed9538408458e87ef
Grzegorz Miebs <grzegorz.miebs@o2.pl>
2022-03-31 15:38:55 +0200

    text edit

--- step7.php
+++ step7.php
@@ -4,14 +4,26 @@
 
 <?php $date = strtotime("+30 day"); ?>
 <?php require_once "includes/checkpoint.php"; ?>
-<?php checkFlow($step, $_SESSION['user'])?>
+<?php checkFlow($step, $_SESSION['user']) ?>
 
 <section class="bg-white">
     <div class="container">
         <div class="row mb-5">
-            <div class="col col-lg-6">
+            <div class="col col-lg-8">
                 <h1 class="display-4 font-weight-bold">Thank You!</h1>
-                <p>You receive a baseline reward of 2 DOT for participating in this study. As mentioned in the introduction, we use this reward to simulate staking rewards based on your final selection of validators. Since this amount is below the staking limit, we will track the average performance (and potential slashes) until the end of the study and thereby estimate your staking rewards theoretically. The final payout (baseline plus staking rewards) will be send to your specified address around the <?php echo date('M d, Y', $date);?>-. If you have any questions, please contact <a href="mailto:experiments@web3.foundation" class="font-weight-bold text-underline" target="_blank">experiments@web3.foundation</a>.</p>
+                <p class="text-justify">You receive a baseline reward of 2 DOT for participating in this study. As mentioned in the
+                    introduction, we use this reward to simulate staking rewards based on your final selection of
+                    validators. Since this amount is below the staking limit, we will track the average performance (and
+                    potential slashes) until the end of the study and thereby estimate your staking rewards
+                    theoretically. The final payout (baseline plus staking rewards) will be send to your specified
+                    address around the <?php echo date('M d, Y', $date); ?>. If you have any questions, please
+                    <nobr>contact <a href="mailto:experiments@web3.foundation"
+                                           class="font-weight-bold text-underline"
+                                           target="_blank">experiments@web3.foundation</a>.
+                    </nobr>
+                </p>
+                <p class="text-justify">Please abstain from discussing this study in public channels as long as it is ongoing, to prevent
+                    other participants being influenced.</p>
                 <p class="mt-5">You can close the webpage now.</p>
             </div>
         </div>
